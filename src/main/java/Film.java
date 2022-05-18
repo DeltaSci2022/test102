87639f2fd6071297f31cee1fcf38ac951474b75b
Simon <simon.svl43@gmail.com>
2022-05-18 15:53:15 +0200

    film object created

---
+++ src/main/java/Film.java
@@ -0,0 +1,61 @@
+public class Film {
+
+    private int rank;
+    private int year;
+    private String[] cast;
+    private String[] director;
+    private String title;
+    private double score;
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String[] getCast() {
+        return cast;
+    }
+
+    public void setCast(String[] cast) {
+        this.cast = cast;
+    }
+
+    public String[] getDirector() {
+        return director;
+    }
+
+    public void setDirector(String[] director) {
+        this.director = director;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+
+
+
+}
