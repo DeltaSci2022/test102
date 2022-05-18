77970c8e28a93cd0c3837e24803e5ed58ba4819f
Simon <simon.svl43@gmail.com>
2022-05-18 17:34:23 +0200

    Changed dependencies to Jsoup

--- src/main/java/WebScraper.java
+++ src/main/java/WebScraper.java
@@ -1,5 +1,5 @@
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 import java.io.IOException;
 
@@ -8,17 +8,12 @@ public class WebScraper {
     // This code will scrape the 250 best movies on IMDB and create movie objects of them.
     private static final String url = "https://www.imdb.com/chart/top/";
 
-    public static void main(String[] args) {
-        WebClient client = new WebClient();
-        client.getOptions().setJavaScriptEnabled(false);
-        client.getOptions().setCssEnabled(false);
+    public static void main(String[] args) throws IOException {
 
-        try {
-            HtmlPage page = client.getPage(url);
-            System.out.println(page.asXml());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        final Document document = Jsoup.connect(url).get();
+
+
+
 
 
 
