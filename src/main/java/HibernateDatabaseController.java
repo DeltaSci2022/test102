5bcef31c324da6185206eb518fbd2b1d0ca04f61
Simon <simon.svl43@gmail.com>
2022-05-24 10:59:48 +0200

    Added the hibernate controller class

--- src/main/java/HibernateDatabaseController.java
+++ src/main/java/HibernateDatabaseController.java
@@ -1,21 +1,45 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class HibernateDatabaseController {
+
+    private SessionFactory factory;
+
 
     public void create(Object obj){
 
+        Session session = factory.getCurrentSession();
+
+        try {
+            // start transaction
+            session.beginTransaction();
+
+            // save the student
+            session.save(obj);
+
+            // commit the transaction
+            session.getTransaction().commit();
+
+        } finally {
+            factory.close();
+        }
+
     }
 
-    public void replace(Object obj){
+//    public Object read(Object obj){
+//
+//    }
+//
+//    public void update(Object obj){
+//
+//    }
+//
+//    public void delete(Object obj){
+//
+//    }
 
+    public HibernateDatabaseController(Object objectToControl) {
+        this.factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(objectToControl.getClass()).buildSessionFactory();;
     }
-
-    public void update(Object obj){
-
-    }
-
-    public void delete(Object obj){
-
-    }
-
-
-
 }
